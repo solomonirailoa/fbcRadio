b9d01c192a395fad4d614539cbbd2d6abe857f5a
Solomoni Railoa <90390564+solomonirailoa@users.noreply.github.com>
2024-06-18 07:50:52 +1200

    Update radio-list.js

--- js/radio-list.js
+++ js/radio-list.js
@@ -1,56 +1,47 @@
-// To add more song, just copy the following code and paste inside the array
+// Radio stations are stored in the current format
 
 //   {
-//     name: "Here is the music name",
-//     artist: "Here is the artist name",
-//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//     name: "Name of the Radio Station",
+//     tag: "Blank. Rather put name of the current song being played by the said radio station",
+//     img: "the source/link of the thumbnail or radio station logo image files",
+//     src: "link to the livestream of the radio station"
 //   }
-
-//paste it inside the array as more as you want music then you don't need to do any other thing
 
 let allStations = [
   {
     name: "Radio Fiji 1",
-    artist: "Na Domoi Viti",
+    tag: "Na Domoi Viti",
     img: "radioFiji1.jpg",
     src: "https://peridot.streamguys1.com:7155/RFOne"
   },
   {
     name: "Radio Fiji 2",
-    artist: "Desh Ki Dhadkan",
+    tag: "Desh Ki Dhadkan",
     img: "radioFiji2.jpg",
     src: "https://peridot.streamguys1.com:7155/RFTwo"
   },
   {
     name: "Bula FM",
-    artist: "Naba dua e na sere",
+    tag: "Naba dua e na sere",
     img: "bulaFM.png",
     src: "https://peridot.streamguys1.com:7155/Bula"
   },
   {
     name: "Mirchi FM",
-    artist: "...it's hot",
+    tag: "...it's hot",
     img: "mirchiFM.png	",
     src: "https://peridot.streamguys1.com:7155/Mirchi"
   },
   {
     name: "Gold FM",
-    artist: "Only the classic hits",
+    tag: "Only the classic hits",
     img: "goldFM.png",
     src: "https://peridot.streamguys1.com:7155/Gold"
   },
   {
     name: "2Day FM",
-    artist: "...rocks. Today's hit music",
+    tag: "...rocks. Today's hit music",
     img: "2dayFM.png",
     src: "https://peridot.streamguys1.com:7155/2Day"
   }
-  // like this paste it and remember to give comma after ending of this bracket }
-  // {
-  //   name: "Here is the music name",
-  //   artist: "Here is the artist name",
-  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
-  // }
 ];
