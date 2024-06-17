c1dbcac108032c0880fc1ea24df48e2d403a228c
Solomoni Railoa <90390564+solomonirailoa@users.noreply.github.com>
2024-06-17 16:50:57 +1200

    Add files via upload

---
+++ js/radio-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allStations = [
+  {
+    name: "Radio Fiji 1",
+    artist: "Na Domoi Viti",
+    img: "radioFiji1.jpg",
+    src: "https://peridot.streamguys1.com:7155/RFOne"
+  },
+  {
+    name: "Radio Fiji 2",
+    artist: "Desh Ki Dhadkan",
+    img: "radioFiji2.jpg",
+    src: "https://peridot.streamguys1.com:7155/RFTwo"
+  },
+  {
+    name: "Bula FM",
+    artist: "Naba dua e na sere",
+    img: "bulaFM.png",
+    src: "https://peridot.streamguys1.com:7155/Bula"
+  },
+  {
+    name: "Mirchi FM",
+    artist: "...it's hot",
+    img: "mirchiFM.png	",
+    src: "https://peridot.streamguys1.com:7155/Mirchi"
+  },
+  {
+    name: "Gold FM",
+    artist: "Only the classic hits",
+    img: "goldFM.png",
+    src: "https://peridot.streamguys1.com:7155/Gold"
+  },
+  {
+    name: "2Day FM",
+    artist: "...rocks. Today's hit music",
+    img: "2dayFM.png",
+    src: "https://peridot.streamguys1.com:7155/2Day"
+  }
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
