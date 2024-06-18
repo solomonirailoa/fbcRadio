3dc3a701c52f975ce1f478c8665e137b48c3f065
Solomoni Railoa <90390564+solomonirailoa@users.noreply.github.com>
2024-06-18 13:18:18 +1200

    Add files via upload

---
+++ dark/js/radio-list.js
@@ -0,0 +1,38 @@
+let allStations = [
+  {
+    name: "Radio Fiji 1",
+    artist: "Na Domoi Viti",
+    img: "radioFiji1.jpg",
+    src: "https://peridot.streamguys1.com:7155/RFOne"
+  },
+  {
+    name: "Radio Fiji 2",
+    artist: "Desh Ki Dhadkan",
+    img: "radioFiji2.jpg",
+    src: "https://peridot.streamguys1.com:7155/RFTwo"
+  },
+  {
+    name: "Bula FM",
+    artist: "Naba dua e na sere",
+    img: "bulaFM.png",
+    src: "https://peridot.streamguys1.com:7155/Bula"
+  },
+  {
+    name: "Mirchi FM",
+    artist: "...it's hot",
+    img: "mirchiFM.png	",
+    src: "https://peridot.streamguys1.com:7155/Mirchi"
+  },
+  {
+    name: "Gold FM",
+    artist: "Only the classic hits",
+    img: "goldFM.png",
+    src: "https://peridot.streamguys1.com:7155/Gold"
+  },
+  {
+    name: "2Day FM",
+    artist: "...rocks. Today's hit music",
+    img: "2dayFM.png",
+    src: "https://peridot.streamguys1.com:7155/2Day"
+  }
+];
